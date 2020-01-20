dcb1c3119369edcbac1b3dbfe0bcafc6d91d7004
drsphelps <drsphelps@gmail.com>
2020-01-20 08:30:36 +0000

    .

---
+++ src/autoencoder.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+from gensim.models import Doc2Vec
+from train_embeddings import MonitorCallback
+from post_cleaning import process_text
+from utils.db import db
+from keras.layers import Input, Dense
+from keras.models import Model
+
+
+# DATA_DIR = 'data/'
+# d2v_model = Doc2Vec.load('data/models/1.modelFile')
+
+
+# vectors = []
+# for d in ['ewhore_data', 'stresser_data']:
+#     directory = DATA_DIR + d
+#     for filename in os.listdir(directory):
+#         with open(directory+'/'+filename, 'r') as f:
+#             v = process_text(f.read())
+#             vectors.append(d2v_model.infer_vector(v))
+
+# np_vs = np.stack(vectors)
+
+# training, test = np_vs[:80, :], np_vs[80:, :]
+
+def build_encoder():
+    dimensions = 500
+    encode = 250
+
+    input_layer = Input(shape=(500, ))
+    L1 = Dense(encode)(input_layer)
+    L2 = Dense(int(encode/2))(L1)
+    L3 = Dense(int(encode/2))(L2)
+    L4 = Dense(encode)(L3)
+
+    model = Model(inputs=input_layer, outputs=L4)
+    return model
+
+
+# print(training.shape)
+# print(test.shape)
+
+posts = []
+conn = db()
+
+for post in conn.get_noncrime_posts(100000):
+    if len(process_text(post[0])) > 10:
+        posts.append(post)
+
+print(posts[:100])
+print(len(posts))
--- src/utils/db.py
+++ src/utils/db.py
@@ -9,8 +9,9 @@ class db:
     def get_conn(self):
         try:
             connection = psycopg2.connect(host="127.0.0.1",
-                                          port="5432",
-                                          database="crimebb")
+                                          port="3333",
+                                          database="crimebb",
+                                          user="drsp2")
         except (Exception, psycopg2.Error) as error:
             print("Error while fetching data from PostgreSQL", error)
 
@@ -21,15 +22,26 @@ class db:
         return self.cursor.fetchall()
 
     def get_posts_from_thread(self, threadId):
-        query = 'SELECT "IdPost", "Content" FROM "Post" WHERE "Thread" = ' + str(threadId) + 'AND "Site" = 0 AND LENGTH("Content") > 100 ORDER BY "IdPost" ASC LIMIT 10;'
+        query = 'SELECT "IdPost", "Content" FROM "Post" WHERE "Thread" = ' + \
+            str(threadId) + 'AND "Site" = 0 AND LENGTH("Content") > 100 ORDER BY "IdPost" ASC LIMIT 10;'
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
     def get_content_from_post(self, postId, siteId):
-        query = 'SELECT "Content" FROM "Post" WHERE "IdPost" = ' + str(postId) + ' AND "Site" = ' + str(siteId) + ';'
+        query = 'SELECT "Content" FROM "Post" WHERE "IdPost" = ' + \
+            str(postId) + ' AND "Site" = ' + str(siteId) + ';'
         self.cursor.execute(query)
         result = self.cursor.fetchall()
         return result[0][0]
+
+    def get_noncrime_posts(self, n):
+        forums = [222, 65, 293, 248, 167, 32, 262, 128]
+        query = '''SELECT DISTINCT p."Content" FROM "Post" p INNER JOIN "Thread" t ON t."IdThread" = p."Thread" WHERE t."Site" = 0 AND t."Forum" = 170 AND LENGTH(p."Content") > 200 AND LOWER(t."Heading") not similar to '%(ewhor|e-whor|hack|crypt|stresser|booter|ddos| rat |dump|phish|exploit|botnet)%' AND LOWER(p."Content") not similar to '%(hidden content.|register)%' LIMIT ''' + str(
+            n)
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print(len(result))
+        return result
 
     def close_connection(self):
         if(self.connection):
